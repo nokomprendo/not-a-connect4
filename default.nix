9ab7e1d7cecac8fc4161e53a93baa43bba7edb95
nokomprendo <nokomprendo@gmail.com>
2021-07-08 10:33:27 +0200

    try ci using nix

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 let
 
-  rev = "d6fe7f78a8739d825ba1176d2d3ecf596368f7a5"; # 2021-05-16
+  rev = "21.05"; # 2021-05-16
+  #rev = "d6fe7f78a8739d825ba1176d2d3ecf596368f7a5"; # 2021-05-16
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz") {};
 
   drv = pkgs.haskellPackages.callCabal2nix "nac4" ./. {};
