ba8503fde4c85a481ab1bc0082aa735ba9c3f09b
nokomprendo <nokomprendo@gmail.com>
2021-07-12 01:09:09 +0200

    fix tests

--- test/NaC4/Server/ModelSpec.hs
+++ test/NaC4/Server/ModelSpec.hs
@@ -18,53 +18,53 @@ spec = do
 
         it "1" $ do
             g <- stToIO $ mkGame PlayerR
-            isInBattle "foo" (Battle "foo" "bar" g 42) `shouldBe` True
+            isInBattle "foo" (Battle "foo" "bar" g 13 37 42) `shouldBe` True
 
         it "2" $ do
             g <- stToIO $ mkGame PlayerR
-            isInBattle "bar" (Battle "foo" "bar" g 42) `shouldBe` True
+            isInBattle "bar" (Battle "foo" "bar" g 13 37 42) `shouldBe` True
 
         it "3" $ do
             g <- stToIO $ mkGame PlayerR
-            isInBattle "baz" (Battle "foo" "bar" g 42) `shouldBe` False
+            isInBattle "baz" (Battle "foo" "bar" g 13 37 42) `shouldBe` False
 
     describe "opponent" $ do
 
         it "1" $ do
             g <- stToIO $ mkGame PlayerR
-            opponent "foo" (Battle "foo" "bar" g 42) `shouldBe` "bar"
+            opponent "foo" (Battle "foo" "bar" g 13 37 42) `shouldBe` "bar"
 
         it "2" $ do
             g <- stToIO $ mkGame PlayerR
-            opponent "bar" (Battle "foo" "bar" g 42) `shouldBe` "foo"
+            opponent "bar" (Battle "foo" "bar" g 13 37 42) `shouldBe` "foo"
 
     describe "Eq battle" $ do
 
         it "1" $ do
             g <- stToIO $ mkGame PlayerR
-            (Battle "foo" "bar" g 42 == Battle "foo" "bar" g 42) `shouldBe` True
+            (Battle "foo" "bar" g 13 37 42 == Battle "foo" "bar" g 13 37 42) `shouldBe` True
 
         it "2" $ do
             g <- stToIO $ mkGame PlayerR
-            (Battle "foo" "bar" g 0 == Battle "foo" "bar" g 42) `shouldBe` True
+            (Battle "foo" "bar" g 0 37 0 == Battle "foo" "bar" g 13 37 42) `shouldBe` True
 
         it "3" $ do
             g <- stToIO $ mkGame PlayerR
-            (Battle "foo" "foo" g 0 == Battle "foo" "bar" g 42) `shouldBe` False
+            (Battle "foo" "foo" g 13 0 0 == Battle "foo" "bar" g 13 37 42) `shouldBe` False
 
         it "4" $ do
             g <- stToIO $ mkGame PlayerR
-            (Battle "bar" "bar" g 0 == Battle "foo" "bar" g 42) `shouldBe` False
+            (Battle "bar" "bar" g 13 37 0 == Battle "foo" "bar" g 13 37 42) `shouldBe` False
 
     describe "updateStats" $ do
 
         it "1" $ do
-            updateStats PlayerR WinR (UserStats 1 2 3 6) `shouldBe` UserStats 2 2 3 7
-            updateStats PlayerR WinY (UserStats 1 2 3 6) `shouldBe` UserStats 1 3 3 7
-            updateStats PlayerR Tie (UserStats 1 2 3 6) `shouldBe` UserStats 1 2 4 7
+            updateStats PlayerR WinR 1 (UserStats 1 2 3 6 1) `shouldBe` UserStats 2 2 3 7 2
+            updateStats PlayerR WinY 0 (UserStats 1 2 3 6 1) `shouldBe` UserStats 1 3 3 7 1
+            updateStats PlayerR Tie 2 (UserStats 1 2 3 6 0) `shouldBe` UserStats 1 2 4 7 2
 
         it "2" $ do
-            updateStats PlayerY WinR (UserStats 1 2 3 6) `shouldBe` UserStats 1 3 3 7
-            updateStats PlayerY WinY (UserStats 1 2 3 6) `shouldBe` UserStats 2 2 3 7
-            updateStats PlayerY Tie (UserStats 1 2 3 6) `shouldBe` UserStats 1 2 4 7
+            updateStats PlayerY WinR 0 (UserStats 1 2 3 6 0) `shouldBe` UserStats 1 3 3 7 0
+            updateStats PlayerY WinY 0 (UserStats 1 2 3 6 0) `shouldBe` UserStats 2 2 3 7 0
+            updateStats PlayerY Tie 0 (UserStats 1 2 3 6 0) `shouldBe` UserStats 1 2 4 7 0
 
