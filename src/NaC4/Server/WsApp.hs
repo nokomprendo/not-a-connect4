8c60108a1b6bef8437c5a6ac188d381ad0ded536
nokomprendo <nokomprendo@gmail.com>
2021-07-16 00:00:07 +0200

    refact wsapp

--- src/NaC4/Server/WsApp.hs
+++ src/NaC4/Server/WsApp.hs
@@ -12,7 +12,7 @@ import Control.Concurrent (threadDelay)
 import Control.Concurrent.STM
 import Control.Exception (finally)
 import Control.Monad (forever, guard)
-import Control.Monad.ST (stToIO)
+import Control.Monad.ST (stToIO, RealWorld)
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
@@ -47,7 +47,28 @@ serverApp modelVar pc = do
             else sendMsg (NotConnected $ user <> " already used") conn
         _ -> T.putStrLn "unknown query"
 
--- TODO refactor
+checkUser :: User -> Model -> Maybe (User, User, Battle, Game RealWorld)
+checkUser user m1 = do
+    let bs = m1^.mBattles & M.filterWithKey (\ k _ -> userInBattleKey user k)
+    guard (M.size bs == 1)
+    let ((userR,userY), bt) = M.elemAt 0 bs
+        g0 = bt^.bGame
+    guard (_currentPlayer g0 == G.PlayerR && user == userR
+            || _currentPlayer g0 == G.PlayerY && user == userY)
+    return (userR, userY, bt, g0)
+
+updateBattle :: User -> User -> Game RealWorld -> Game RealWorld 
+             -> Double -> Double -> Model -> Model
+updateBattle userR userY g0 g1 time1 time2 m =
+    m & mBattles %~ M.mapWithKey (\(ur,uy) bt -> 
+        if userR==ur && userY==uy
+        then 
+            let dt = time1 - bt^.bTimeI
+            in if _currentPlayer g0 == G.PlayerR
+                then bt & bGame.~g1 & bTimeI.~time2 & bTimeR+~dt
+                else bt & bGame.~g1 & bTimeI.~time2 & bTimeY+~dt
+        else bt)
+
 run :: TVar Model -> User -> WS.Connection -> IO ()
 run modelVar user conn = forever $ do
     msg <- recvMsg conn
@@ -55,40 +76,27 @@ run modelVar user conn = forever $ do
         Just (P.PlayMove move) -> do
             time1 <- myGetTime
             m1 <- readTVarIO modelVar
-            let bs = m1^.mBattles & M.filterWithKey (\ k _ -> userInBattleKey user k)
-            if M.size bs /= 1
-            then T.putStrLn $ "invalid playmove from " <> user
-            else 
-                let ((userR,userY), bt0@(Battle g0 _ _ _)) = M.elemAt 0 bs
-                in if (_currentPlayer g0 == G.PlayerR && user /= userR)
-                        || (_currentPlayer g0 == G.PlayerY && user /= userY)
-                    then T.putStrLn $ "not your turn " <> user
+            case checkUser user m1 of
+                Nothing -> T.putStrLn $ "invalid playmove from " <> user
+                Just (userR, userY, bt, g0) -> do
+                    g1 <- stToIO $ G.playJ move g0
+                    time2 <- myGetTime
+                    atomically $ modifyTVar' modelVar 
+                        (updateBattle userR userY g0 g1 time1 time2)
+                    let connR = (m1^.mClients) M.! userR
+                        connY = (m1^.mClients) M.! userY
+                        conn1 = if G._currentPlayer g1 == G.PlayerR
+                                then connR else connY
+                    (board, player, status) <- stToIO $ P.fromGame g1
+                    if G.isRunning g1
+                    then sendMsg (GenMove board player status) conn1
                     else do
-                        g1 <- stToIO $ G.playJ move g0
-                        time2 <- myGetTime
-                        atomically $ modifyTVar' modelVar $ \m -> 
-                            m & mBattles %~ M.mapWithKey (\(ur,uy) bt -> 
-                                if userR==ur && userY==uy
-                                then 
-                                    let dt = time1 - bt^.bTimeI
-                                    in if _currentPlayer g0 == G.PlayerR
-                                        then bt & bGame.~g1 & bTimeI.~time2 & bTimeR+~dt
-                                        else bt & bGame.~g1 & bTimeI.~time2 & bTimeY+~dt
-                                else bt)
-                        let connR = (m1^.mClients) M.! userR
-                            connY = (m1^.mClients) M.! userY
-                        let conn1 = if G._currentPlayer g1 == G.PlayerR
-                                    then connR else connY
-                        (board, player, status) <- stToIO $ P.fromGame g1
-                        if G.isRunning g1
-                        then sendMsg (GenMove board player status) conn1
-                        else do
-                            T.putStrLn $ userR <> " vs " <> userY 
-                                <> " -> " <> T.pack (show status)
-                            sendMsg (EndGame board PlayerR status) connR
-                            sendMsg (EndGame board PlayerY status) connY
-                            finishBattle modelVar (userR,userY) bt0 board status
-        _ -> putStrLn "unknown message; skipping"
+                        T.putStrLn $ userR <> " vs " <> userY 
+                            <> " -> " <> T.pack (show status)
+                        sendMsg (EndGame board PlayerR status) connR
+                        sendMsg (EndGame board PlayerY status) connY
+                        finishBattle modelVar (userR,userY) bt board status
+        _ -> putStrLn "unknown message"
 
 stop :: TVar Model -> User -> IO ()
 stop modelVar user = do
